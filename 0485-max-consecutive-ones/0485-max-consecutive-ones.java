4951c671a6e36514370f50b46af7f1ae5c1afbeb
Suraj-chauhan18 <suraj0.3351d@gmail.com>
2026-08-14 14:28:04 +0530

    Time: 3 ms (59.34%), Space: 52.5 MB (46.48%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max=0;
+        int left=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                left=i+1;
+            }
+            max=Math.max(max,i-left+1);
+        }
+        return max;
+    }
+}
