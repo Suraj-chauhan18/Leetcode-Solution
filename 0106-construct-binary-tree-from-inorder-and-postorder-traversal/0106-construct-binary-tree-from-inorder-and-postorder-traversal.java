56daf5c1c976125f9dc73400f323d1a7beb2c2dc
Suraj-chauhan18 <suraj0.3351d@gmail.com>
2026-08-27 00:42:44 +0530

    Time: 3 ms (23.05%), Space: 45.6 MB (81.82%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -14,21 +14,19 @@
  * }
  */
 class Solution {
-    int postidx;
+    int postidx=0;
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-       postidx=inorder.length-1;
+       postidx=postorder.length-1;
        return solve(inorder,postorder,0,inorder.length-1);
     }
     public TreeNode solve(int[] inorder,int[]postorder,int left,int right){
          if(left>right) return null;
-         int rootval=postorder[postidx];
-         TreeNode root =new TreeNode(rootval);
+         int rootval=postorder[postidx--];
+         TreeNode root=new TreeNode(rootval);
          int idx=left;
-          postidx--;
          while(inorder[idx]!=rootval){
             idx++;
          }
-        
          root.right=solve(inorder,postorder,idx+1,right);
          root.left=solve(inorder,postorder,left,idx-1);
          return root;
