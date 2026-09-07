ed5729963e5a22813ad9692e7fde12b4c4b4b5d6
Suraj-chauhan18 <suraj0.3351d@gmail.com>
2026-09-07 22:43:52 +0530

    Time: 1 ms (96%), Space: 46.1 MB (84.82%) - LeetHub

--- 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.java
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.java
@@ -1,28 +1,24 @@
 class Solution {
     public boolean checkValidGrid(int[][] grid) {
-        int n=grid.length;
-        if(grid[0][0]!=0){
-            return false;
-        }
-        return solve(0,0,1,grid,n);
+      int n=grid.length;
+      if(grid[0][0]!=0){
+        return false;
+      }
+     return solve(0,0,1,grid);
     }
-    public boolean solve(int r,int c,int num,int[][]grid,int n){
-        if(num==n*n){
-            return true;
-        }
-        int[][] moves={{-2,-1},{-2,1},{-1,-2},{1,-2},{2,-1},{2,1},{-1,2},{1,2}};
-
-        for(int[] move:moves){
+    public boolean solve(int r,int c,int num,int[][]grid){  int n=grid.length;
+        if(num==n*n) return true;
+        int[][] moves={{-2,-1},{-2,1},{-1,-2},{1,-2},{-1,2},{1,2},{2,-1},{2,1}};
+        for(int [] move:moves){
             int newr=r+move[0];
             int newc=c+move[1];
-
-            if(newr>=0 && newr<n && newc>=0 && newc<n && grid[newr][newc]==num){
-                if(solve(newr,newc,num+1,grid,n)){
+           if(newr>=0 && newr<n && newc>=0 && newc<n && grid[newr][newc]==num){
+            if(solve(newr,newc,num+1,grid)){
                 return true;
-                }
             }
+           }
+           
         }
-        return false;
+         return false;
     }
-    
 }
