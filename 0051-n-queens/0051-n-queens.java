cac35ce89c3bd2f457282c8e51e0ac38fb1b0463
Suraj-chauhan18 <suraj0.3351d@gmail.com>
2026-09-07 23:17:30 +0530

    Time: 2 ms (81.43%), Space: 46.6 MB (64.23%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,43 +1,42 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> list=new ArrayList<>();
-        char[][] board=new char[n][n];
-        for(char[]row:board){
+        char [][] board=new char[n][n];
+        for(char [] row : board){
             Arrays.fill(row,'.');
         }
-        solve(0,list,board,n);
+        solve(0,list,n,board);
         return list;
     }
-    public void solve(int row,List<List<String>> list,char[][]board,int n){
-        if(row==n){
+    public void solve(int r,List<List<String>> list,int n,char[][]board){
+        if(r==n){
             List<String> ans=new ArrayList<>();
-            for(char[] r:board){
-                ans.add(new String(r));
-            }
-            list.add(ans);
-            return;
+             for(char[]ch:board){
+                ans.add(new String(ch));
+             }
+             list.add(ans);
+             return;
         }
-        for(int col=0;col<n;col++){
-            if(possible(row,col,board,n)){
-                board[row][col]='Q';
-                solve(row+1,list,board,n);
-                board[row][col]='.';
-            }
+        for(int c=0;c<n;c++){
+           if(possible(r,c,n,board)){
+            board[r][c]='Q';
+            solve(r+1,list,n,board);
+            board[r][c]='.';
+           } 
         }
     }
-    public boolean possible(int row,int col,char[][]board,int n){
-        
-        for(int i=row-1;i>=0;i--){
-            if(board[i][col]=='Q'){
+    public boolean possible(int r,int c,int n,char[][]board){
+        for(int i=r;i>=0;i--){
+            if(board[i][c]=='Q'){
                 return false;
             }
         }
-        for(int i=row-1, j=col-1;i>=0 && j>=0;i--,j--){
+        for(int i=r-1,j=c-1 ; i>=0 && j>=0 ; i--,j--){
             if(board[i][j]=='Q'){
                 return false;
             }
         }
-        for(int i=row-1,j=col+1;i>=0 && j<n;i--,j++){
+        for(int i=r-1,j=c+1; i>=0 && j<n; i--,j++){
             if(board[i][j]=='Q'){
                 return false;
             }
