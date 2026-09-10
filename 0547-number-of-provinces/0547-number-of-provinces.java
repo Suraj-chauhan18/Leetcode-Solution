e3c46855640b45a61219a6fcbb98fb30fb4b3d70
Suraj-chauhan18 <suraj0.3351d@gmail.com>
2026-09-10 22:53:42 +0530

    Time: 1 ms (85.84%), Space: 47.4 MB (43.58%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,33 +1,22 @@
 class Solution {
     public int findCircleNum(int[][] edges) {
-        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
-        for(int i=0;i<edges.length;i++){
-            adj.add(new ArrayList<>());
-        }
-      for(int i=0;i<edges.length;i++){
-        for(int j=0;j<edges.length;j++){
-            if(edges[i][j]==1)
-            adj.get(i).add(j);
-        }
-      }
-        
-        boolean[]visited=new boolean[edges.length];
+       boolean[]visited=new boolean[edges.length];
         int count=0;
         for(int i=0;i<edges.length;i++){
             if(!visited[i]){
-                dfs(i,adj,visited);
+                dfs(i,edges,visited);
                 count++;
             }
         }
         
        return count; 
     }
-    public void dfs(int n, ArrayList<ArrayList<Integer>> adj,boolean[]visited){
+    public void dfs(int n, int[][] edges,boolean[]visited){
         
             visited[n]=true;
-            for(int nbr:adj.get(n)){
-                if(!visited[nbr]){
-                    dfs(nbr,adj,visited);
+            for(int nbr=0;nbr<edges.length;nbr++){
+                if( edges[n][nbr]==1 && !visited[nbr]){
+                    dfs(nbr,edges,visited);
                 }
             }
     }
