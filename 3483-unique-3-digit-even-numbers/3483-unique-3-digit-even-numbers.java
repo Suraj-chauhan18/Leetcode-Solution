0e47bd94459964364c38b902f20117814635d1a9
Suraj-chauhan18 <suraj0.3351d@gmail.com>
2026-09-12 00:29:25 +0530

    Time: 7 ms (34.93%), Space: 46.2 MB (58.53%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -2,18 +2,24 @@ class Solution {
     public int totalNumbers(int[] digits) {
         int n=digits.length;
         HashSet<Integer> set=new HashSet<>();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-            for(int k=0;k<n;k++){
-                if(i==j || j==k || i==k) continue;
-                if(digits[i]==0) continue;
-            int a=digits[i]*100+digits[j]*10+digits[k];
-            if(a%2==0){
-                set.add(a);
-            }
-        }
-            }
-        }
+        boolean[] used=new boolean[n];
+        solve(digits,0,0,set,used);
         return set.size();
+    }
+    public void solve(int[]digits,int idx,int num,HashSet<Integer>set,boolean[]used){
+        if(idx==3){
+            set.add(num);
+            return;
+            
+        }
+        
+        for(int i=0;i<digits.length;i++){
+           if(used[i]) continue;
+           if(idx==0 && digits[i]==0) continue;
+           if (idx == 2 && digits[i] % 2 != 0) continue;
+           used[i]=true;
+           solve(digits,idx+1,num*10+digits[i],set,used);
+           used[i]=false;
+        }
     }
 }
